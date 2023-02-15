0d6d95e77ce1f0e71ef676641924e4fb598d8811
jgpawletko <jgpawletko@gmail.com>
2023-02-14 21:39:26 -0500

    add setupMuxMultiPath() and handleFuncBuilderReturnFileContents()

--- viewer/test_helpers.go
+++ viewer/test_helpers.go
@@ -16,6 +16,27 @@ func setupMux(apiPath string, filePath string) (mux *http.ServeMux) {
 	return mux
 }
 
+func handleFuncBuilderReturnFileContents(path string) func(http.ResponseWriter, *http.Request) {
+	filePath := path
+	return func(w http.ResponseWriter, _ *http.Request) {
+		data, _ := os.ReadFile(filePath)
+		w.Write(data)
+	}
+}
+
+// This function takes an array of 2-string arrays
+// where:
+//
+//	the first element of the 2-string array is the API path
+//	the second element of the 2-string array is the path of the file to serve at the API path
+func setupMuxMultiPath(aPathsfPaths [][2]string) (mux *http.ServeMux) {
+	mux = http.NewServeMux()
+
+	for _, apfp := range aPathsfPaths {
+		mux.HandleFunc(apfp[0], handleFuncBuilderReturnFileContents(apfp[1]))
+	}
+	return mux
+}
 func setupTestServerClient(ts *httptest.Server) {
 	c := new(Config)
 	c.BaseURL = ts.URL
