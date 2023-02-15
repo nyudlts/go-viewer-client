fb3b3c6b3f87e9d1e4659731abaf0eec6d0a60e0
jgpawletko <jgpawletko@gmail.com>
2023-02-14 22:17:52 -0500

    update version timestamp

--- viewer/viewer.go
+++ viewer/viewer.go
@@ -4,7 +4,7 @@ import (
 	client "github.com/nyudlts/go-rsbe-client/rsbe"
 )
 
-const Version = "0.1.0"
+const Version = "0.2.0-20230213T10_17"
 
 // Map client package types, functions into this package
 type Config = client.Config
